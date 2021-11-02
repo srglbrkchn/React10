5a2e3fed3dabb1e9b574f87acfd11460287d0f8f
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-02 08:51:56 +0100

    Import everything from math.js using wild card *.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pi, {doublePi, triplePi} from "./math.js";
+import * as pi from "./math.js";
+
+console.log(pi);
 
 ReactDOM.render(
     <ul>
-    <li>{pi}</li>
-    <li>{doublePi()}</li>
-    <li>{triplePi()}</li>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
 </ul>, document.getElementById("root"));
